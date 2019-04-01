9649faef6bc08cbeb0354d1dc12af8c5cd47ce90
lucabelezal <lucabelezal@gmail.com>
2019-03-31 23:53:00 -0300

    Add project

---
+++ MainApp/MainApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainApp
+//
+//  Created by Lucas Nascimento on 31/03/19.
+//  Copyright © 2019 Lucas Nascimento. All rights reserved.
+//
+
+import Common
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
